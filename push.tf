a4d0e69e81899bb28d9396fda74d9c3246e2a780
manoj <ankireddy828@gmail.com>
2023-09-05 13:57:49 +0530

    register

---
+++ push.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "push_to_registry" {
+  triggers = {
+    image_id = docker_image.zoo.image_id
+  }
+
+  provisioner "local-exec" {
+    command = "docker push 000100000/terraform:latest" # Replace with your registry URL and image name
+  }
+}
