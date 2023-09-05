7ae1d45de3e04643989aaaa5732d4dd7dd3b38e2
manoj <ankireddy828@gmail.com>
2023-09-05 14:04:59 +0530

    resgir

---
+++ rt.tf
@@ -0,0 +1,7 @@
+resource "null_resource" "docker_login" {
+  provisioner "local-exec" {
+  command = <<EOT
+    echo "${var.password}" | docker login -u "${var.username}" --password-stdin
+  EOT
+  }
+}
